47fb496dce8c7bc799fdd793d9e4d76742385e3a
saJeremyQin <343220868@qq.com>
2023-03-03 15:54:53 +1030

    Initial commit

---
+++ App.js
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Login from './Screens/Login.js';
+import Home from './Screens/Home.js';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createStackNavigator();
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Login" 
+      >
+        <Stack.Screen 
+          name="Login" 
+          component={Login}
+          options={
+            {
+              headerShown:false
+            }
+          }
+          />
+        <Stack.Screen 
+          name="Home" 
+          component={Home}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
